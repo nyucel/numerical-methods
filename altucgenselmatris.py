69948975380b09dc37d89179d8a540ddf44061b4
hakanbisgin <hakanbisgin@yahoo.com.tr>
2017-03-21 13:35:14 +0200

    Update altucgenselmatris.py

--- altucgenselmatris.py
+++ altucgenselmatris.py
@@ -10,8 +10,19 @@ for line in dosya.readlines():
 dosya.close()
 boyut = len(matris)
 
+
+
 for n in range(boyut):
-    kat = int(matris[n][n])
+    kat = float(matris[n][n])
+    degis = n
+    while kat == 0:
+        degis = degis +1
+        if degis==boyut-1:
+            print('Hata: Satir islemlerinde sıfır(0) hatasi. Matrisi cozume ulastiramadim.')
+        matris[n],matris[degis]=matris[degis],matris[n]
+        kat = float(matris[degis][degis])
+    print (matris)
+
     for m in range(boyut+1):
         matris[n][m]=int(matris[n][m])/kat
     for p in range(1,boyut-n):
@@ -19,4 +30,9 @@ for n in range(boyut):
         for q in range(boyut+1):
             matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
 
+for n in range (boyut-1,0,-1):
+    for m in range (1,n+1,+1):
+        matris[n-m][n] , matris[n-m][n+1] = matris[n-m][n]-matris[n-m][n]*matris[n][n] , matris[n-m][n+1]-matris[n-m][n+1]*matris[n][n+1]
+
 print(matris)
+    
