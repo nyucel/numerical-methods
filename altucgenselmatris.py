255f0c48513e0b80f3db8f9682471a10b439fff9
erenkundakci <ernkundakci@gmail.com>
2017-03-21 19:15:26 +0200

    ustucgenselmatris yapımı
    üst üçgensel matris yapımı ve değişkenlerin sonucunun ekrana bastırılması

--- altucgenselmatris.py
+++ altucgenselmatris.py
@@ -12,6 +12,15 @@ boyut = len(matris)
 
 for n in range(boyut):
     kat = int(matris[n][n])
+    if(kat == 0):
+        i=1
+        while(n+i < boyut):
+            matris[n] , matris[n+i] = matris[n+i] , matris[n]
+            kat = int(matris[n][n])
+            i = i + 1
+        if(kat == 0):
+            print("Bir satır başka bir satırın katı... Çıkılıyor...")
+            exit()
     for m in range(boyut+1):
         matris[n][m]=int(matris[n][m])/kat
     for p in range(1,boyut-n):
@@ -19,4 +28,12 @@ for n in range(boyut):
         for q in range(boyut+1):
             matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
 
+for i in range(boyut-1,-1,-1):
+    for k in range(1,i+1):
+        kat = float(matris[i-k][i])
+        for l in range(boyut+1):
+            matris[i-k][l] = float(matris[i-k][l])-float(matris[i][l])*(kat/float(matris[i][i]))
+
 print(matris)
+for i in range(1,boyut+1):
+    print('x{0} = {1}'.format(i, matris[i-1][boyut]))
