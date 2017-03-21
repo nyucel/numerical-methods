4f511dd3a1d87d6c5edbaf32cb134af3c60746c1
numanstc <numan_satici@hotmail.com>
2017-03-21 21:50:10 +0200

    Ustucgensel matris
    Ustucgensel matris eklendi

--- altucgenselmatris.py
+++ altucgenselmatris.py
@@ -18,5 +18,13 @@ for n in range(boyut):
         kat = float(matris[n+p][n])
         for q in range(boyut+1):
             matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
-
+            
+for n in range(boyut-1,-1,-1):
+    for satir in range(0,n): #üst satırları döndür
+        print(satir)
+        kat = matris[n-1-satir][n]
+        print(kat)
+        for m in range(n,boyut+1):
+            matris[n-1-satir][m] -= matris[n][m]*kat
+            
 print(matris)
