6110865cd478a095da1ebe59ea92e620a0aece73
Can Alpay Çiftçi <canalpayciftci@gmail.com>
2017-03-27 04:48:31 +0300

    update nonlineer-3.py

--- nonlineer-3.py
+++ nonlineer-3.py
@@ -5,9 +5,30 @@ def f(x,y):
 def g(x,y):
     return(y+3*x*y**2-57)
 
+
+def fx(x, y):
+    return 2*x+y
+
+
+def gx(x, y):
+    return 3*y**2
+
+
+def fy(x, y):
+    return x
+
+
+def gy(x, y):
+    return 1+6*x*y
+
 xi = float(input("x için başlangıç değerini girin: "))
 yi = float(input("y için başlangıç değerini girin: "))
 
 
+for i in range(20):
+    xi = xi - ((f(xi, yi)*gy(xi, yi) - g(xi, yi)*fy(xi, yi)) / (fx(xi, yi)*gy(xi, yi)
+                                                                - fy(xi, yi)*gx(xi, yi)))
+    yi = yi - ((g(xi, yi)*fx(xi, yi) - f(xi, yi)*gx(xi, yi)) / (fx(xi, yi)*gy(xi, yi)
+                                                                - fy(xi, yi)*gx(xi, yi)))
 
 print(xi,yi)
