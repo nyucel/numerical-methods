03f7d32f0db9c23182f51e16039bdec1d05157a7
Gamze Gülyol <ggulyol@gmail.com>
2017-03-29 17:02:31 +0300

    ödev-3

--- polinom-butunkokler.py
+++ polinom-butunkokler.py
@@ -3,7 +3,47 @@
 def f(x):
     return(1*x**5-7*x**4-3*x**3+79*x**2-46*x-120)
 
+def fonksiyon(dizi,xi):
+    toplam=0
+    j=0
+    for i in range(len(dizi)-1,-1,-1):
+        toplam=toplam+(dizi[j]*(xi**i))
+        j=j+1
+    return toplam
+def fi(dizi,xi):
+    toplam=0
+    j=0
+    for i in range(len(dizi)-1,0,-1):
+        toplam=toplam+(dizi[j]*i*(xi**(i-1)))
+        j=j+1
+    return toplam
+
+def kokBul(dizi,xi):
+    for i in range(10000):
+        xi=xi-fonksiyon(dizi,xi)/fi(dizi,xi)
+    return xi
+
+derece=int(input("Kaçıncı dereceden polinom girmek istiyorsunuz?"))
+
+dizi=[]
+sonuc=[]
+for i in range(derece+1):
+
+    print("katsayıları giriniz")
+    katsayı=int(input())
+    dizi.append(katsayı)
+print(dizi)
+
 xi = float(input("bir başlangıç değeri girin: "))
 
+while(len(dizi)>2):
+    sonuc.append(xi)
+    for i in range(1,len(dizi)-1):
+        dizi[i]=dizi[i]+(dizi[i-1]*xi)
 
-print(x1,x2,x3,x4,x5)
+    dizi.pop()
+    xi=kokBul(dizi,xi)
+xi=-dizi[1]*dizi[0]
+sonuc.append(xi)
+print(sonuc)
+
