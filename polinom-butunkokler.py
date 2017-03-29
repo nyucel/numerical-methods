674bcca8818060848e857647e39c688b05725472
hakan <the_big_storm18@hotmail.com>
2017-03-30 00:19:40 +0300

    polinom-butunkokler.py update

--- polinom-butunkokler.py
+++ polinom-butunkokler.py
@@ -1,9 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-def f(x):
-    return(1*x**5-7*x**4-3*x**3+79*x**2-46*x-120)
+dosya = open("katsayilar.txt")
+for dizi in dosya.readlines():
+	dizi = dizi.rstrip('\n').split(' ')
+	dosya.close()
+	boyut = len(dizi)
+print(dizi)
+for i in range(boyut):
+	dizi[i] = int(dizi[i])
 
-xi = float(input("bir başlangıç değeri girin: "))
+def f(x, dizi):
+	toplam = 0
+	index = 0
+	for i in range(boyut-1, -1, -1):
+		toplam = toplam+(dizi[index]) * pow(x,i)
+		index = index+1
+	return(toplam)
 
+def fi(x, dizi):
+	toplam = 0
+	index = 0
+	for i in range(boyut-1, -1, -1):
+		if(i == 0):
+			break
+		toplam = toplam + (dizi[index]) * pow(x, i-1) * i
+		index = index + 1
+	return(toplam)
 
-print(x1,x2,x3,x4,x5)
+def kokbul(xi, dizi):    
+	for i in range(25):
+		x2 = xi - f(xi, dizi) / fi(xi, dizi)
+		xi = x2
+	return(xi)
+
+def polinombol(xi, dizi):
+	for i in range(boyut):
+		if(i == 0):
+			continue
+			dizi[i] = dizi[i-1] * xi + dizi[i]
+	return xi
+
+xi = float(input("Bir değer girin :")) 
+i = 1
+while(boyut > 1):
+	print("%d.kök"%(i), polinombol(kokbul(xi, dizi), dizi))
+	i = i + 1
+	print(dizi)
+	boyut = boyut - 1
