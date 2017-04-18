e3f31ae055fe45e548033c5b40866af171f1e0ac
Alim Öncül <aoncul@stu.comu.edu.tr>
2017-04-18 16:34:29 +0300

    lagrange.py tamamlandı
    carpim üst taraf, bolum alt taraf, sonuc görüntüsüyle çarpılmış hali

--- lagrange.py
+++ lagrange.py
@@ -9,5 +9,22 @@ for line in dosya.readlines():
     degerler.append(line)
 dosya.close()
 
-x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+x = float(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+uzunluk = len(degerler[0])
+for m in range(len(degerler)):   #degerler dizisini float'a çeviriyoruz
+    for n in range(uzunluk):
+        degerler[m][n]=float(degerler[m][n])
 
+toplam= 0.0
+for i in range (uzunluk):
+    carpim = 1.0
+    bolum = 1.0
+    for k in range (uzunluk):
+        if(k==i):
+            continue
+        else:
+            carpim=carpim*(x-degerler[0][k])
+            bolum=bolum*(degerler[0][i]-degerler[0][k])
+            sonuc=(carpim/bolum)*degerler[1][i]
+    toplam=toplam+sonuc
+print (toplam)
