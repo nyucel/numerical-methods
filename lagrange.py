2ed2975d3f531eda4b173189734da0b27f920965
ilhanlinur <ilhanlinurr@gmail.com>
2017-04-19 20:56:35 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -10,4 +10,20 @@ for line in dosya.readlines():
 dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+cozum=0
+satir=len(degerler)
+sutun=len(degerler[0])
+for i in range(sutun):
+    pay=1
+    payda=1
+    for j in range(sutun):
+        if(i==j):
+            continue
+            else:
+                pay=pay*(x-degerler[0][j])
+                payda=payda*(degerler[0][i]-degerler[0][k])
+                cozum=cozum((pay/payda)*degerler[1][i])
+                
+        
+print(cozum)
 
