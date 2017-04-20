b8a970c28737b8cac3b950a16f3a448e93ed1a21
neslihanaydin <neslihanaydin43@gmail.com>
2017-04-20 20:45:37 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+def P(liste,x):
+    fonksiyon=0.0
+    # sayac=0 ,f(x) degerinin kacinci deger oldugu
+    for sayac in range(len(liste[0])):
+        pay=1.0
+        payda=1.0
+        for i in range(0,len(liste[0])):
+            if (sayac==i):
+                i=i+1
+                if (i>=len(liste[0])):
+                    continue;
+                    
+            pay = pay*(x-int(liste[0][i]))
+        for h in range(0,len(liste[0])):
+            if (sayac==h):
+                h=h+1
+                if (h>=len(liste[0])):
+                    continue;
+                
+            payda = payda*(int(liste[0][sayac])-int(liste[0][h]))
+        fonksiyon = fonksiyon + (pay*int(liste[1][sayac]))/payda
+    return (fonksiyon)
 dosya=open("lagrange.txt")
-degerler = []
+liste = []
 
 for line in dosya.readlines():
     line = line.rstrip('\n').split(' ')
-    degerler.append(line)
+    liste.append(line)
+boyut=len(liste[0])
 dosya.close()
 
-x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+while True:
+    x = float(input("hangi degerin hesaplanmasini istiyorsunuz: "))
+    print (P(liste,x))
 
