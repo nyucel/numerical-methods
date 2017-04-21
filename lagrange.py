285318861392afa2de75f456fc48479929996acd
esrakarakas <esrakarakas96@gmail.com>
2017-04-22 02:20:42 +0300

    lagrange cozumu

--- lagrange.py
+++ lagrange.py
@@ -15,8 +15,18 @@ boyut=len(line)
 carpim=1
 carpim2=1
 j=0
+toplam=0
+
+for i in range(0,2):
+	for j in range(0,boyut):
+		degerler[i][j]=int(degerler[i][j])
+
+
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+
+
 for n in range(boyut-1):
+	j=n
 	if n==0:
 		for i in range(boyut-1):
 			carpim=carpim*(x-degerler[0][i+1])
@@ -25,31 +35,29 @@ for n in range(boyut-1):
 		toplam=toplam+(carpim/carpim2)*(degerler[1][0])
 		carpim=1
 		carpim2=1
-              
-	elif n==(boyut-1):
+      
+	elif n==boyut-1:
 		for i in range(boyut-2):
 			carpim=carpim*(x-degerler[0][i])
-                for i in range(boyut-2):
+		for i in range(boyut-2):
 			carpim2=carpim2*(degerler[0][n]-degerler[0][i])
 		toplam=toplam+(carpim/carpim2)*(degerler[1][n])
 		carpim=1
 		carpim2=1
 
-	j=n
-		
 	else: 
 		while j>0:
-			carpim=carpim*(x-degerler[0][j-1]
-			j=j-1	
+			carpim=carpim*(x-degerler[0][j-1])
+			j-=1	
 		j=n
 		while j>0:
-			carpim2=carpim2*(degerler[0][n]-degerler[0][j-1]
+			carpim2=carpim2*(degerler[0][n]-degerler[0][j-1])
 			j=j-1
 		j=n	
 		while j<boyut-1:
 			carpim=carpim*(x-degerler[0][j+1])
-			j=j+1
-                j=n
+			j+=1
+		j=n
 		while j<boyut-1:
 			carpim2=carpim2*(degerler[0][n]-degerler[0][j+1])
 			j=j+1
@@ -60,7 +68,4 @@ for n in range(boyut-1):
 
 
 
-		
-	
-
-x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+print(toplam)
