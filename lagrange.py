27cf358e56fe0d46fd3bf3c27f97066b2de04e72
Özgür Bulut <ozgurbulut0@outlook.com>
2017-04-21 22:21:45 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -11,3 +11,21 @@ dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
 
+
+Boyut=len(degerler[0])
+for i in range(len(degerler)):
+    for j in range(Boyut):
+        degerler[i][j]=float(degerler[i][j])
+
+Sonuc=0
+for i in range(Boyut):
+    carpim1=1
+    carpim2=1
+    for k in range(Boyut):
+        if(k==i):
+            continue
+        else:
+            carpim1=carpim1*(x-degerler[0][k])
+            carpim2=carpim2*(degerler[0][i]-degerler[0][k])
+    Sonuc=Sonuc+((carpim1/carpim2)*degerler[1][i])
+print(Sonuc)
