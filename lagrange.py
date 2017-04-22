2878db79852aef58eb8f69c857d6a2cb48925f8e
Ege  Burak Özpınar <ege_ozpinar@hotmail.com>
2017-04-22 13:45:06 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -11,3 +11,25 @@ dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
 
+uzunluk=len(degerler[0])
+
+for i in range(len(degerler)):
+    for j in range(uzunluk):
+        degerler[i][j] = float(degerler[i][j])
+
+toplam = 0.0
+
+for i in range(uzunluk):
+    carpim = 1.0
+    bolum = 1.0
+    for k in range(uzunluk):
+        if (k==i):
+            continue
+        else:
+            carpim *= (x-degerler[0][k])
+            bolum *= (degerler[0][i] - degerler[0][k])
+            sonuc = (carpim/bolum) * degerler[1][i]
+
+    toplam += sonuc
+
+print(toplam)
