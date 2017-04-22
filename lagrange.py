e119df47a9747b89f8186250e1980790771ffffd
mehmet canak <mehmet.canak.36@gmail.com>
2017-04-22 14:07:31 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -11,3 +11,18 @@ dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
 
+boyut=len(degerler[0])  
+for a in range(len(degerler)):
+     for b in range(boyut):
+        degerler[a][b]=float(degerler[a][b])
+
+for i in range(boyut):
+      pay=1
+    payda=1
+      for j in range(boyut):
+        if(j!=i):
+                 pay *=(x-degerler[0][j])
+                 payda *=(degerler[0][i]-degerler[0][j])
+    sonuc +=((pay/payda)*degerler[1][i])
+
+print sonuc 
