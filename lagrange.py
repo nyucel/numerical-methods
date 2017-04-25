eaed64392372824ca9da0c4bad176a0b6ef8fd0e
hakanbisgin <hakanbisgin@yahoo.com.tr>
2017-04-25 14:29:36 +0300

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -9,23 +9,4 @@ for line in dosya.readlines():
     degerler.append(line)
 dosya.close()
 
-x = float(input("hangi değerin hesaplanmasını istiyorsunuz: "))
-
-xi = degerler[0]
-yi = degerler[1]
-n = len(xi)
-
-sonuc = 0
-for i in range(n):
-    ekle, pay, payda = 0, 1, 1
-    for j in range(0,n,1):
-        if i==j:
-            continue
-        else:
-            pay *= (x - float(xi[j]))
-            payda *= (float(xi[i]) - float(xi[j]))
-        ekle += float(yi[i]) * pay / payda
-    sonuc += ekle
-
-print ("X = ", x, " sonuc = ", sonuc)
-    
+x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
