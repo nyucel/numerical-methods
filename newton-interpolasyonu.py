8a12c9cf875393e9a3c5483fd0f5754a0e11c20a
muratboy <muratboy.comu@gmail.com>
2017-04-27 17:09:12 +0300

    Update newton-interpolasyonu.py

--- newton-interpolasyonu.py
+++ newton-interpolasyonu.py
@@ -6,8 +6,19 @@ degerler = []
 
 for line in dosya.readlines():
     line = line.rstrip('\n').split(' ')
+    line = [float(i) for i in line]
     degerler.append(line)
 dosya.close()
+arasonuc,sonuc,boyut,sf = 1,degerler[1][0],len(degerler[0]),[]
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
 
+sf.append(degerler[1])
+for i in range(boyut-1):
+    a = []
+    for j in range(len(sf[i])-1):
+        a.append((sf[i][j]-sf[i][j+1])/(degerler[0][j]-degerler[0][j+i+1]))
+    sf.append(a)
+    arasonuc *= x-degerler[0][i]
+    sonuc += arasonuc*sf[i+1][0]
+print sonuc
