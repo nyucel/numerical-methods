642902eabe45f56bb011156712f1393fa78e1ca4
neslihanaydin <neslihanaydin43@gmail.com>
2017-04-28 17:40:55 +0300

    Update newton-interpolasyonu.py

--- newton-interpolasyonu.py
+++ newton-interpolasyonu.py
@@ -1,13 +1,45 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 dosya=open("lagrange.txt")
-degerler = []
+d = []
+e=[-1] 
 
 for line in dosya.readlines():
     line = line.rstrip('\n').split(' ')
-    degerler.append(line)
+    d.append(line)
 dosya.close()
+#print(d)
 
-x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+def bolunmusfark(a,e,boyut):
+    b=a
+    i=0
+    c=[]
+    c=e
+    e=[]
+    
+    while((boyut-1-a)>0):
+        e.append((float(c[i])-float(c[i+1]))/(float(d[0][i])-float(d[0][b+1])))
+        b=b+1
+        i=i+1
+        a=a+1
+    return(e)
+x = float(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+fonksiyon = float(d[1][0])
+carpim=1.0
+boyut=len(d[0]) #4
+e=d[1]
+for a in range(boyut-1):
+    u=bolunmusfark(a,e,boyut)
+    carpim=carpim*(x-float(d[0][a]))
+    #print("\n")
+    e=[]
+    e=u
+    #print(e)
+    fonksiyon = fonksiyon + carpim*u[0]
 
+print(fonksiyon)
+
+
+
+        
+    
+
+    
