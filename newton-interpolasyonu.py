d66b8ce3f03e1eeb5ee767e5de9abdafd777a2b9
ilhanlinur <ilhanlinurr@gmail.com>
2017-04-28 10:28:11 +0300

    Update newton-interpolasyonu.py

--- newton-interpolasyonu.py
+++ newton-interpolasyonu.py
@@ -10,4 +10,30 @@ for line in dosya.readlines():
 dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
-
+row=len(degerler)
+col=len(degerler[0])
+afark=[]
+bfark=[]
+sonuc=degerler[1][0]
+for i in range(row):
+    for j in range(column)
+    degerler[i][j]=float(degerler[i][j])
+    sayac=1
+for i in range (col-1):
+    for j in range(column-1):
+         if(i==0):
+                bfark.append(float((degerler[1][j+1])-(degerler[1][j])/(degerler[0][j+1])-(degerler[0][j])))
+                afark[0]=bfark[0]
+                if(i!=0):
+                    bfark[j]=((bfark[j+1])-(bfark[j]))/((degerler[0][j+sayac])-(degerler[0][j]))
+                    afark[i]=bfark[0]
+                    sayac=sayac+1
+                    column=column-1
+                    for i in range(column-1):
+                        carpim=1
+                        for j in range(i+1):
+                            carpim=1
+                            for j in range(i+1):
+                                carpim=carpim*afark[i]
+                                sonuc=sonuc+carpim
+                                print(sonuc)
