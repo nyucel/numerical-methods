6827459f1d8ab5fb7f7563b2d76d1ac8383224ec
Ege  Burak Özpınar <ege_ozpinar@hotmail.com>
2017-04-30 00:10:03 +0300

    Update newton-interpolasyonu.py

--- newton-interpolasyonu.py
+++ newton-interpolasyonu.py
@@ -10,4 +10,27 @@ for line in dosya.readlines():
 dosya.close()
 
 x = int(input("hangi değerin hesaplanmasını istiyorsunuz: "))
+x = float(input("hangi değerin hesaplanmasını istiyorsunuz: "))
 
+for i in range(len(degerler)):
+    for j in range(len(degerler[0])):
+        degerler[i][j] = float(degerler[i][j])
+
+n = len(degerler[0])
+carpim = 1
+toplam = 0
+sonluFark = []
+nSonlu = len(degerler[0])
+fark = 1
+sonluFark.append(degerler[1])
+for i in range(n-1):
+    temp = []
+    for j in range(nSonlu-1):
+        temp.append((sonluFark[i][j+1]-sonluFark[i][j])/(degerler[0][j+fark]-degerler[0][j]))
+    sonluFark.append(temp)
+    nSonlu -= 1
+    fark += 1
+    carpim *= (x-degerler[0][i])
+    toplam += carpim*sonluFark[i+1][0]
+toplam += degerler[1][0]
+print("Sonuc:",toplam)
